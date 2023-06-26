dace8069b90b09e84289ef4ce46c0a728a7200a9
REQUENA Lucas <120340380+rqlcs@users.noreply.github.com>
2023-06-26 18:49:07 +0200

    Create index.js

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var pageContent = document.querySelector("body");
+    setTimeout(function() {
+        pageContent.classList.add("fade-in");
+    }, 500);
+};Update 
